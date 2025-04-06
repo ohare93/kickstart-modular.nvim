cb53ecd8c484984021e4c7b7c7f3fed0fc1005dc
Jordan Munch O'Hare <git@jordan.munchohare.com>
2025-04-06 17:54:14 +0200

    start of own setup

---
+++ start.sh
@@ -0,0 +1,14 @@
+
+
+# https://linuxize.com/post/how-to-install-gcc-on-ubuntu-20-04/
+sudo apt update
+sudo apt install build-essential
+
+sudo apt install make
+sudo apt install unzip
+brew install ripgrep
+
+# https://github.com/dahromy/nerd-fonts-manager
+sudo apt-get install -y curl jq wget unzip parallel imagemagick fontconfig
+
+sudo apt install fonts-noto-color-emoji
