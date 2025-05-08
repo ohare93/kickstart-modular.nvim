13483dde581580298adc7330f0fc22fbfbb32e28
Jordan Munch OHare <git@jordan.munchohare.com>
2025-05-08 14:29:23 +0200

    unimpaired

---
+++ lua/custom/plugins/unimpaired.lua
@@ -0,0 +1,9 @@
+return {
+  'tummetott/unimpaired.nvim',
+  event = 'VeryLazy',
+  opts = {
+    -- add options here if you wish to override the default settings
+    default_keymaps = true,
+    keymaps = {},
+  },
+}
