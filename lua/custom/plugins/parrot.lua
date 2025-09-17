bd5572a371e7286b699b3f57d5caafa7f2902fa9
Jordan Munch O'Hare <jmh@danskecommodities.com>
2025-09-17 10:20:21 +0200

    chore: parrot stuff again

--- lua/custom/plugins/parrot.lua
+++ lua/custom/plugins/parrot.lua
@@ -60,145 +60,145 @@ return {
         },
       },
     },
-    chat_dir = "~/Development/ai/parrot/chats",
-    state_dir = "~/Development/ai/parrot/state",
+    -- chat_dir = "~/Development/ai/parrot/chats",
+    -- state_dir = "~/Development/ai/parrot/state",
     hooks = {
       Complete = function(prt, params)
         local template = [[
-        I have the following code from {{filename}}:
+          I have the following code from {{filename}}:
 
-        ```{{filetype}}
-        {{selection}}
-        ```
+          ```{{filetype}}
+          {{selection}}
+          ```
 
-        Please finish the code above carefully and logically.
-        Respond just with the snippet of code that should be inserted."
-        ]]
+          Please finish the code above carefully and logically.
+          Respond just with the snippet of code that should be inserted."
+          ]]
         local model_obj = prt.get_model 'command'
         prt.Prompt(params, prt.ui.Target.append, model_obj, nil, template)
       end,
       CompleteFullContext = function(prt, params)
         local template = [[
-        I have the following code from {{filename}}:
+          I have the following code from {{filename}}:
 
-        ```{{filetype}}
-        {{filecontent}}
-        ```
+          ```{{filetype}}
+          {{filecontent}}
+          ```
 
-        Please look at the following section specifically:
-        ```{{filetype}}
-        {{selection}}
-        ```
+          Please look at the following section specifically:
+          ```{{filetype}}
+          {{selection}}
+          ```
 
-        Please finish the code above carefully and logically.
-        Respond just with the snippet of code that should be inserted.
-        ]]
+          Please finish the code above carefully and logically.
+          Respond just with the snippet of code that should be inserted.
+          ]]
         local model_obj = prt.get_model 'command'
         prt.Prompt(params, prt.ui.Target.append, model_obj, nil, template)
       end,
       CompleteMultiContext = function(prt, params)
         local template = [[
-        I have the following code from {{filename}} and other realted files:
+          I have the following code from {{filename}} and other realted files:
 
-        ```{{filetype}}
-        {{multifilecontent}}
-        ```
+          ```{{filetype}}
+          {{multifilecontent}}
+          ```
 
-        Please look at the following section specifically:
-        ```{{filetype}}
-        {{selection}}
-        ```
+          Please look at the following section specifically:
+          ```{{filetype}}
+          {{selection}}
+          ```
 
-        Please finish the code above carefully and logically.
-        Respond just with the snippet of code that should be inserted.
-        ]]
+          Please finish the code above carefully and logically.
+          Respond just with the snippet of code that should be inserted.
+          ]]
         local model_obj = prt.get_model 'command'
         prt.Prompt(params, prt.ui.Target.append, model_obj, nil, template)
       end,
       Explain = function(prt, params)
         local template = [[
-        Your task is to take the code snippet from {{filename}} and explain it with gradually increasing complexity.
-        Break down the code's functionality, purpose, and key components.
-        The goal is to help the reader understand what the code does and how it works.
+          Your task is to take the code snippet from {{filename}} and explain it with gradually increasing complexity.
+          Break down the code's functionality, purpose, and key components.
+          The goal is to help the reader understand what the code does and how it works.
 
-        ```{{filetype}}
-        {{selection}}
-        ```
+          ```{{filetype}}
+          {{selection}}
+          ```
 
-        Use the markdown format with codeblocks and inline code.
-        Explanation of the code above:
-        ]]
+          Use the markdown format with codeblocks and inline code.
+          Explanation of the code above:
+          ]]
         local model = prt.get_model 'command'
         prt.logger.info('Explaining selection with model: ' .. model.name)
         prt.Prompt(params, prt.ui.Target.new, model, nil, template)
       end,
       FixBugs = function(prt, params)
         local template = [[
-        You are an expert in {{filetype}}.
-        Fix bugs in the below code from {{filename}} carefully and logically:
-        Your task is to analyze the provided {{filetype}} code snippet, identify
-        any bugs or errors present, and provide a corrected version of the code
-        that resolves these issues. Explain the problems you found in the
-        original code and how your fixes address them. The corrected code should
-        be functional, efficient, and adhere to best practices in
-        {{filetype}} programming.
+          You are an expert in {{filetype}}.
+          Fix bugs in the below code from {{filename}} carefully and logically:
+          Your task is to analyze the provided {{filetype}} code snippet, identify
+          any bugs or errors present, and provide a corrected version of the code
+          that resolves these issues. Explain the problems you found in the
+          original code and how your fixes address them. The corrected code should
+          be functional, efficient, and adhere to best practices in
+          {{filetype}} programming.
 
-        ```{{filetype}}
-        {{selection}}
-        ```
+          ```{{filetype}}
+          {{selection}}
+          ```
 
-        Fixed code:
-        ]]
+          Fixed code:
+          ]]
         local model_obj = prt.get_model 'command'
         prt.logger.info('Fixing bugs in selection with model: ' .. model_obj.name)
         prt.Prompt(params, prt.ui.Target.new, model_obj, nil, template)
       end,
       Optimize = function(prt, params)
         local template = [[
-        You are an expert in {{filetype}}.
-        Your task is to analyze the provided {{filetype}} code snippet and
-        suggest improvements to optimize its performance. Identify areas
-        where the code can be made more efficient, faster, or less
-        resource-intensive. Provide specific suggestions for optimization,
-        along with explanations of how these changes can enhance the code's
-        performance. The optimized code should maintain the same functionality
-        as the original code while demonstrating improved efficiency.
+          You are an expert in {{filetype}}.
+          Your task is to analyze the provided {{filetype}} code snippet and
+          suggest improvements to optimize its performance. Identify areas
+          where the code can be made more efficient, faster, or less
+          resource-intensive. Provide specific suggestions for optimization,
+          along with explanations of how these changes can enhance the code's
+          performance. The optimized code should maintain the same functionality
+          as the original code while demonstrating improved efficiency.
 
-        ```{{filetype}}
-        {{selection}}
-        ```
+          ```{{filetype}}
+          {{selection}}
+          ```
 
-        Optimized code:
-        ]]
+          Optimized code:
+          ]]
         local model_obj = prt.get_model 'command'
         prt.logger.info('Optimizing selection with model: ' .. model_obj.name)
         prt.Prompt(params, prt.ui.Target.new, model_obj, nil, template)
       end,
       UnitTests = function(prt, params)
         local template = [[
-        I have the following code from {{filename}}:
+          I have the following code from {{filename}}:
 
-        ```{{filetype}}
-        {{selection}}
-        ```
+          ```{{filetype}}
+          {{selection}}
+          ```
 
-        Please respond by writing table driven unit tests for the code above.
-        ]]
+          Please respond by writing table driven unit tests for the code above.
+          ]]
         local model_obj = prt.get_model 'command'
         prt.logger.info('Creating unit tests for selection with model: ' .. model_obj.name)
         prt.Prompt(params, prt.ui.Target.enew, model_obj, nil, template)
       end,
       Debug = function(prt, params)
         local template = [[
-        I want you to act as {{filetype}} expert.
-        Review the following code, carefully examine it, and report potential
-        bugs and edge cases alongside solutions to resolve them.
-        Keep your explanation short and to the point:
+          I want you to act as {{filetype}} expert.
+          Review the following code, carefully examine it, and report potential
+          bugs and edge cases alongside solutions to resolve them.
+          Keep your explanation short and to the point:
 
-        ```{{filetype}}
-        {{selection}}
-        ```
-        ]]
+          ```{{filetype}}
+          {{selection}}
+          ```
+          ]]
         local model_obj = prt.get_model 'command'
         prt.logger.info('Debugging selection with model: ' .. model_obj.name)
         prt.Prompt(params, prt.ui.Target.enew, model_obj, nil, template)
@@ -210,74 +210,74 @@ return {
           return
         else
           local template = [[
-          I want you to act as a commit message generator. I will provide you
-          with information about the task and the prefix for the task code, and
-          I would like you to generate an appropriate commit message using the
-          conventional commit format. Do not write any explanations or other
-          words, just reply with the commit message.
-          Start with a short headline as summary but then list the individual
-          changes in more detail.
+            I want you to act as a commit message generator. I will provide you
+            with information about the task and the prefix for the task code, and
+            I would like you to generate an appropriate commit message using the
+            conventional commit format. Do not write any explanations or other
+            words, just reply with the commit message.
+            Start with a short headline as summary but then list the individual
+            changes in more detail.
 
-          Here are the changes that should be considered by this message:
-          ]] .. vim.fn.system 'git diff --no-color --no-ext-diff --staged'
+            Here are the changes that should be considered by this message:
+            ]] .. vim.fn.system 'git diff --no-color --no-ext-diff --staged'
           local model_obj = prt.get_model 'command'
           prt.Prompt(params, prt.ui.Target.append, model_obj, nil, template)
         end
       end,
       SpellCheck = function(prt, params)
         local chat_prompt = [[
-        Your task is to take the text provided and rewrite it into a clear,
-        grammatically correct version while preserving the original meaning
-        as closely as possible. Correct any spelling mistakes, punctuation
-        errors, verb tense issues, word choice problems, and other
-        grammatical mistakes.
-        ]]
+          Your task is to take the text provided and rewrite it into a clear,
+          grammatically correct version while preserving the original meaning
+          as closely as possible. Correct any spelling mistakes, punctuation
+          errors, verb tense issues, word choice problems, and other
+          grammatical mistakes.
+          ]]
         prt.ChatNew(params, chat_prompt)
       end,
       CodeConsultant = function(prt, params)
         local chat_prompt = [[
-          Your task is to analyze the provided {{filetype}} code and suggest
-          improvements to optimize its performance. Identify areas where the
-          code can be made more efficient, faster, or less resource-intensive.
-          Provide specific suggestions for optimization, along with explanations
-          of how these changes can enhance the code's performance. The optimized
-          code should maintain the same functionality as the original code while
-          demonstrating improved efficiency.
+            Your task is to analyze the provided {{filetype}} code and suggest
+            improvements to optimize its performance. Identify areas where the
+            code can be made more efficient, faster, or less resource-intensive.
+            Provide specific suggestions for optimization, along with explanations
+            of how these changes can enhance the code's performance. The optimized
+            code should maintain the same functionality as the original code while
+            demonstrating improved efficiency.
 
-          Here is the code
-          ```{{filetype}}
-          {{filecontent}}
-          ```
-        ]]
+            Here is the code
+            ```{{filetype}}
+            {{filecontent}}
+            ```
+          ]]
         prt.ChatNew(params, chat_prompt)
       end,
       ProofReader = function(prt, params)
         local chat_prompt = [[
-        I want you to act as a proofreader. I will provide you with texts and
-        I would like you to review them for any spelling, grammar, or
-        punctuation errors. Once you have finished reviewing the text,
-        provide me with any necessary corrections or suggestions to improve the
-        text. Highlight the corrected fragments (if any) using markdown backticks.
+          I want you to act as a proofreader. I will provide you with texts and
+          I would like you to review them for any spelling, grammar, or
+          punctuation errors. Once you have finished reviewing the text,
+          provide me with any necessary corrections or suggestions to improve the
+          text. Highlight the corrected fragments (if any) using markdown backticks.
 
-        When you have done that subsequently provide me with a slightly better
-        version of the text, but keep close to the original text.
+          When you have done that subsequently provide me with a slightly better
+          version of the text, but keep close to the original text.
 
-        Finally provide me with an ideal version of the text.
+          Finally provide me with an ideal version of the text.
 
-        Whenever I provide you with text, you reply in this format directly:
+          Whenever I provide you with text, you reply in this format directly:
 
-        ## Corrected text:
+          ## Corrected text:
 
-        {corrected text, or say "NO_CORRECTIONS_NEEDED" instead if there are no corrections made}
+          {corrected text, or say "NO_CORRECTIONS_NEEDED" instead if there are no corrections made}
 
-        ## Slightly better text
+          ## Slightly better text
 
-        {slightly better text}
+          {slightly better text}
 
-        ## Ideal text
+          ## Ideal text
 
-        {ideal text}
-        ]]
+          {ideal text}
+          ]]
         prt.ChatNew(params, chat_prompt)
       end,
     },
